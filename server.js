1ce2fc2cbfad16984a248fa67572a2c5603f7881
Nolan Picini <nolanpic@gmail.com>
2020-02-26 12:43:14 -0800

    Config and run server

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const usersRouter = require('./users/router');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/users', usersRouter);
+
+module.exports = server;
